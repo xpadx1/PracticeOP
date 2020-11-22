f71da3b112eee87841ea4dcff746c0182ad0d708
ipad2013q <71705996+ipad2013q@users.noreply.github.com>
2020-11-22 16:53:02 +0200

    Create Object1.js

---
+++ 2 - Basic Syntax/Object1.js
@@ -0,0 +1,15 @@
+function fn() {
+    const user = {
+        name: "mark"
+    }
+    let user1 = {
+        name: "John"
+    }
+   user.name = "Petro"
+   user1.name = "Tommas"
+    
+    console.log(user.name);
+    console.log(user1.name)
+}
+fn()
+
