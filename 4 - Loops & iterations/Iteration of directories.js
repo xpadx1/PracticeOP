4db3a77584dbc50ca729a6a0286d8f997c7e07c2
Arsenii <71705996+xpadx1@users.noreply.github.com>
2021-01-03 21:27:30 +0200

    Update Iteration of directories.js

--- 4 - Loops & iterations/Iteration of directories.js
+++ 4 - Loops & iterations/Iteration of directories.js
@@ -1,6 +1,17 @@
-const people {
-    {Lenin: born: 1870, died: 1924},
-    {Hitler: born: 1889, died: 1945},
-    {Stalin: born: 1878, died: 1953},
+const people = {
+    Lenin: {born: 1870, died: 1924},
+    Hitler: {born: 1889, died: 1945},
+    Stalin: {born: 1878, died: 1953},
 };
+
+for (let key in people){
+    let ages = people[key].died - people[key].born;
+    people[key].age = ages
+}
+
+console.table(people)
+
+
+
+
 
