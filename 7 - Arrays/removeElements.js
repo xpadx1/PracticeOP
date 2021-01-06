f05e3304078e69ee18aacf319d4e251b78771b77
Arsenii <71705996+xpadx1@users.noreply.github.com>
2021-01-06 17:41:27 +0200

    Create removeElements.js

---
+++ 7 - Arrays/removeElements.js
@@ -0,0 +1,15 @@
+function removeElement(arr, num, num1, num2){
+    let result = [];
+    for (let i = 1; i <= arr.length; i++){
+       if (arr[i] != arr[num]){
+         result.push(arr[i-1]);
+       }else{
+           continue;
+       }
+    }
+    console.log(result);
+}
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+
+
