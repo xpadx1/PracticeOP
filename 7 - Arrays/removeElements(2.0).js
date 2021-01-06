cb010f60761881e625cab24ff1b01a87428ea087
Arsenii <71705996+xpadx1@users.noreply.github.com>
2021-01-06 17:47:49 +0200

    Update removeElements(2.0).js

--- 7 - Arrays/removeElements(2.0).js
+++ 7 - Arrays/removeElements(2.0).js
@@ -1,7 +1,7 @@
 function removeElement(arr, num, num1, num2){
     let result = [];
     for (let i = 1; i <= arr.length; i++){
-       if (arr[i] != arr[num] || arr[i] != arr[num1] ){
+       if (arr[i] != arr[num] && arr[i] != arr[num1] && arr[i] != arr[num2]){
          result.push(arr[i-1]);
        }else{
            continue;
@@ -10,4 +10,4 @@ function removeElement(arr, num, num1, num2){
     console.log(result);
 }
 const array = [1, 2, 3, 4, 5, 6, 7];
-removeElement(array, 5);
+removeElement(array, 5, 1, 3); // 2,4,6,7
